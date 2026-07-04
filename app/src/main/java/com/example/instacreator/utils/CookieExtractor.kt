36760c67453b069f643dc6fe694d0d14f3b00a3c
tawfiq22x <108868414+tawfiq22x@users.noreply.github.com>
2026-07-04 14:51:45 +0300

    Update CookieExtractor.kt

--- app/src/main/java/com/example/instacreator/utils/CookieExtractor.kt
+++ app/src/main/java/com/example/instacreator/utils/CookieExtractor.kt
@@ -6,14 +6,15 @@ import org.instagram4j.Instagram4j
 object CookieExtractor {
     fun extract(client: Instagram4j): Map<String, String> {
         val cookies = mutableMapOf<String, String>()
-        client.client.httpClient.cookieJar.let { jar ->
-            jar.loadForRequest(HttpUrl.get("https://i.instagram.com")).forEach { cookie ->
-                when (cookie.name) {
-                    "sessionid" -> cookies["sessionid"] = cookie.value
-                    "csrftoken" -> cookies["csrftoken"] = cookie.value
-                    "ds_user_id" -> cookies["ds_user_id"] = cookie.value
-                    "ig_did" -> cookies["ig_did"] = cookie.value
-                }
+        val httpClient = client.client.httpClient
+        val cookieJar = httpClient.cookieJar
+        val url = HttpUrl.parse("https://i.instagram.com")!!
+        cookieJar.loadForRequest(url).forEach { cookie ->
+            when (cookie.name) {
+                "sessionid" -> cookies["sessionid"] = cookie.value
+                "csrftoken" -> cookies["csrftoken"] = cookie.value
+                "ds_user_id" -> cookies["ds_user_id"] = cookie.value
+                "ig_did" -> cookies["ig_did"] = cookie.value
             }
         }
         return cookies
