222508acc8a1d32185560866b0cf75b14e8fe281
tawfiq22x <108868414+tawfiq22x@users.noreply.github.com>
2026-07-04 14:52:52 +0300

    Update AccountDatabase.kt

--- app/src/main/java/com/example/instacreator/data/database/AccountDatabase.kt
+++ app/src/main/java/com/example/instacreator/data/database/AccountDatabase.kt
@@ -4,6 +4,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import android.content.Context
+@Database(entities = [AccountEntity::class], version = 1, exportSchema = false)
 
 @Database(entities = [AccountEntity::class], version = 1)
 abstract class AccountDatabase : RoomDatabase() {
